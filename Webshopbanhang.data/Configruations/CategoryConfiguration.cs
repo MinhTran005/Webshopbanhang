00971a73ef46190080649e8f1e29063e8bbe834a
Trần Văn Minh <minhruoi001@outlook.com>
2022-01-13 16:14:51 +0700

    database_design

---
+++ Webshopbanhang.data/Configruations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshopbanhang.data.Entities;
+using Webshopbanhang.data.Enums;
+
+namespace Webshopbanhang.data.Configruations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+
+        }
+    }
+}
---
+++ Webshopbanhang.data/Configruations/ProductConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshopbanhang.data.Entities;
+
+namespace Webshopbanhang.data.Configruations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Price).IsRequired();
+            builder.Property(x => x.OrigingalPrice).IsRequired();
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.ViewCount).IsRequired().HasDefaultValue(0);
+           
+        }
+    }
+}
